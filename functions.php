a90cd38e9239b6f51b095981db40cddb419dad6a
Dillon Caldwell <dcaldwell1228@gmail.com>
2022-11-30 12:17:16 -0500

    remove unecessary js & css files

--- functions.php
+++ functions.php
@@ -13,3 +13,25 @@ function my_theme_enqueue_styles() {
         $theme->get('Version') // this only works if you have Version in the style header
     );
 }
+
+
+// Remove emoji support
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+// Remove rss feed links
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+remove_action( 'wp_head', 'feed_links', 2 );
+
+// Remove wp-embed
+add_action( 'wp_footer', function(){
+    wp_dequeue_script( 'wp-embed' );
+});
+
+
+add_action( 'wp_enqueue_scripts', function(){
+    // Remove block library css
+    wp_dequeue_style( 'wp-block-library' );
+    // Remove comment reply JS
+    wp_dequeue_script( 'comment-reply' );
+} );
